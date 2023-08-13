51adc7b4fcdd933d352f1d6c2e9e663f536d48b7
vemlier <mck7944@naver.com>
2023-08-13 21:31:35 +0900

    add: oauth20

---
+++ Oauth/src/auth/auth.controller.ts
@@ -0,0 +1,19 @@
+import { Controller, Get, Req, UseGuards } from '@nestjs/common';
+import { Request } from 'express';
+import { GoogleAuthGuard } from './utils/Guards';
+
+@Controller('auth')
+export class AuthController {
+  @Get('google/login')
+  @UseGuards(GoogleAuthGuard)
+  handleLogin() {
+    return { msg: 'Google Authentication' };
+  }
+
+  // api/auth/google/redirect
+  @Get('google/redirect')
+  @UseGuards(GoogleAuthGuard)
+  handleRedirect() {
+    return { msg: 'OK' };
+  }
+}
